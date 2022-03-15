8ba05c4b887b3b1aac156146b2218fef7f072ca7
Vanshika Gupta <86125131+vanshikagupta174@users.noreply.github.com>
2022-03-15 23:38:24 +0530

    Create AboutUs.js

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,58 @@
+
+import '../styles/Aboutus.css';
+
+
+const Aboutus = () => {
+    return (
+        <div>
+            <div className="mainsection">
+                <div className="firstcontainer">
+                    <div>
+                        <h3 className="mainheading">
+                            Urja Global Limited
+                        </h3>
+                        <hr width="60px" height="2px" color="#fe7701" />
+                        <h5 className="subheading">
+                            <b>One of India’s leading Renewable Energy developers.</b>
+                        </h5>
+                    </div>
+                    <p>
+                        Urja Global Limited is in one of India’s leading Renewable Energy developers and operators. Urja is engaged in design, consultancy, integration, supply, installation, commissioning & maintenance of off-grid and grid connected Solar Power Plants. We are approved Channel Partners of Ministry of New and Renewable Energy (MNRE), Govt. of India.
+                    </p>
+                </div>
+                <div className="secondcontainer">
+                    <div className="scon">
+                        <p>
+                            Our team comprises of seasoned professionals with a proven track record in design and integration of off grid and grid connected solar projects.
+                        </p>
+                        <p>
+                            We own one of the largest sales/installation networks PAN India backed by our team.Urja Global Limited is promoted by the promoters group hailing from Rajasthan, India.
+                        </p>
+                        <p>
+                            With more than two decades of experience, they have established as one of the country’s leading industrial companies and a strong well-diversified conglomerate which is active in: Renewable Energy, IT&ITES, Tours & Travels, and Education & Training having presence across the globe.
+                        </p>
+                        <p>
+                            We assure customer’s satisfaction in terms of product quality and service. Urja offers outstanding quality varied Solar Products which makes use of green and clean energy simple viz. Solar Home Lighting Systems,
+                        </p>
+                    </div>
+                    <div className="scont">
+                        <iframe width="650" height="300" src="https://www.youtube.com/embed/utmFXSZAo58" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div className="thirdcontainer">
+                    <p>
+                        Solar Lanterns, Solar Street Lighting Systems, Roof Top Systems, Solar PV Pannels with quality and standards conforming to ISO 9001 . All our Solar products range comply with MNRE standards.
+                    </p>
+                    <p>
+                        We at Urja are committed to providing world-class services to rapidly develop rural India focusing on rural electrification especially in hilly areas, forest regions, deserts, islands, far flung villages, unmanned locations and other areas which require reliable and uninterrupted power supply.
+                    </p>
+                    <p>
+                        With a strong design and manufacturing back ground UGL has developed themselves as a leading manufacturer of Solar Energy Systems to cater to almost the complete range of customer requirements
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Aboutus;
