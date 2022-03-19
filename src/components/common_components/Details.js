7e1626de4c858d63a4a78cb856e601f8e160bb04
Vanshika Gupta <86125131+vanshikagupta174@users.noreply.github.com>
2022-03-19 20:05:36 +0530

    Update Details.js

--- src/components/common_components/Details.js
+++ src/components/common_components/Details.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Details = () => {
+const Details =props => {
   return (
     <>
 
-      <div className='heading'>Urja Global Limited Board of Director</div>
+      <div className='heading'>Urja Global Limited {props.details_title}</div>
       <div className='sub-heading'>Urja Global Limited is in one of India’s leading Renewable Energy developers.</div>
       <p className='paragraph-css'>Urja Global Limited is in one of India’s leading Renewable Energy developers and operators. Urja is engaged in design, consultancy, integration, supply, installation, commissioning & maintenance of off-grid and grid connected Solar Power Plants. We are approved Channel Partners of Ministry of New and Renewable Energy (MNRE), Govt. of India.</p>
 
