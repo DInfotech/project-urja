5ddc0a58ddca5679a7302b48330a0fd3a97e7f4e
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2022-03-21 00:01:23 +0530

    footer

--- src/components/footer/FooterPage.js
+++ src/components/footer/FooterPage.js
@@ -12,83 +12,89 @@ import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-
+import { FiMail} from 'react-icons/fi';
+import { IoLocationSharp} from "react-icons/io5";
+import { IoCallOutline} from "react-icons/io5";
 const year = new Date().getFullYear();
 
 const FooterPage = () => {
   return (
     <div class="footer-bg">
 
-    <Container>
-  <Row>
-    <Col>
-      <h5 class="footer-title" > Contact Us</h5>
-      <p class="footer-content"><HomeSharpIcon/>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-       Integer vel magna quis quam mollis rutrum. Nam aliquam pretium nibh, nec rhoncus magna posuere id.
-     </p>
+      <Container>
+        <Row>
+          <Col>
 
-     <p><CallSharpIcon/>+91-9000030240</p>
-    </Col>
+            <h4 class="footer-title" >Useful Links</h4>
+            <p class="footer-content">
+              <ul type="none">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Career</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Privacy & Policy</a></li>
+                <li><a href="#">Refund Policy</a></li>
 
-    <Col>
-    <h5 class="footer-title" >Online Shopping</h5>
-    <p class="footer-content">
-      <ul type="none">
-        <li><a href="#">Men</a></li>
-        <li><a href="#">WoMen</a></li>
-        <li><a href="#">Kids</a></li>
-        <li><a href="#">Beauty</a></li>
-        <li><a href="#">Gift Cards</a></li>
-        <li><a href="#">Home & Living</a></li>
-      </ul>
-    </p>
+              </ul>
+            </p>
+          </Col>
 
-    </Col>
+          <Col>
+            <h4 class="footer-title" >Corporate Address</h4>
+            <p class="footer-content">
+              <ul type="none">
+                <li><a href="#"><CallSharpIcon />+91-11-25279143 | 45588275</a></li>
+                <li><a href="#"><FiMail/>  info@urjaglobal.in</a></li>
+                <li><a href="#"><IoLocationSharp/>487/63, 1st Floor, National Market, Peeragarhi, New Delhi-110087
+                </a></li>
+                <li><a href="#"><CallSharpIcon /> Toll Free: 8000-500-789</a></li>
 
-    <Col>
-    <h5 class="footer-title" >Useful Links</h5>
-    <p class="footer-content">
-      <ul type="none">
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#">T&C</a></li>
-        <li><a href="#">Terms of Use</a></li>
-        <li><a href="#">Track Orders </a></li>
-        <li><a href="#">Shipping</a></li>
-        <li><a href="#">Privacy Policy</a></li>
-        <li><a href="#">Blog</a></li>
-      </ul>
-    </p>
+              </ul>
+            </p>
 
-    </Col>
+          </Col>
 
-    <Col>
-    <p style={{margin:'2vw 0 0 2vw'}}><CheckCircleSharpIcon/>100% ORIGINAL gaurantee for all products at phoenixfashion.com</p>
+          <Col >
+            <h4 class="footer-title" >Factory Address</h4>
+       
+            <p class="footer-content">
+              <ul type="none">
+              <li><a href="#"><CallSharpIcon /> +91-11-25279143 | 45588275</a></li>
+                <li><a href="#"><FiMail/>  info@urjaglobal.in</a></li>
+                <li><a href="#"><IoLocationSharp/> Plot No.11, Sec.16, HSIIDC, Industrial complex, Bahadurgarh, Distt. Jhajjar, Haryana – 124507, India
+                </a></li>
+                <li><a href="#"><IoLocationSharp/> Urja Incubation/Training Center:
+487/63, 1st Floor, National Market, Peeragarhi, New Delhi-87</a></li>
 
-    <p style={{margin:'1vw 0 0 2vw'}}><AirportShuttleSharpIcon/>Return within 30 days of recieving your order</p>
-    <h5 style={{margin:'1vw 0 0 2vw'}}>Keep In Touch</h5>
-    <p style={{margin:'0.5rem 0 0 2vw'}}>
-    <span class="social-icon"><FacebookSharpIcon/></span>
-    <span class="social-icon"><YouTubeIcon/></span>
-    <span class="social-icon"><InstagramIcon/></span>
-    <span class="social-icon"><TwitterIcon/></span>
+              </ul>
+            </p>
 
-    </p>
-    </Col>
-  </Row>
-</Container>
-     <div style={{marginTop : "2rem"}}>
-      <h5  style={{color:"rgb(73, 72, 72)"}} className="end-footer"> © Copyright Phoenix {year}</h5>
-     </div>
-  
-</div>
+
+          </Col>
+
+          <Col >
+          <h4 class="footer-title" >Contact Us</h4>
+           
+            {/* <h4 style={{ margin: '1vw 0 0 2vw' }}>Keep In Touch</h4> */}
+            <p style={{ margin: '0',padding:"0", color:"white" }}>
+              <span class="social-icon"><FacebookSharpIcon /></span>
+              <span class="social-icon"><YouTubeIcon /></span>
+              <span class="social-icon"><InstagramIcon /></span>
+              <span class="social-icon"><TwitterIcon /></span>
+
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <hr style={{color:"white",marginTop: "0rem"}}></hr>
+      <div style={{ marginTop: "1rem" }}>
+        <p style={{ color: "rgb(73, 72, 72)",marginBottom:"0" }} className="end-footer"> © Urja Global Limited 2020 | CIN NO: L67120DL1992PLC048983 | GSTIN NO: 07AAACC0367M1ZN </p>
+      </div>
+
+    </div>
 
   );
 }
 
 export default FooterPage;
 
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// Integer vel magna quis quam mollis rutrum. Nam aliquam pretium nibh, nec rhoncus magna posuere id.
-// Vestibulum enim ante, porta ac neque ac, vehicula facilisis dolor. 
-// Nulla quis consequat neque, et cursus orci. 
-// Suspendisse sit amet dui semper, rhoncus lectus non, efficitur risus
