2b2f204991671711f4ba03be7d3068d8b04f5d07
saksham295 <sakshamjain114@gmail.com>
2022-03-25 20:36:06 +0530

    (fix): conflicts

--- src/components/common_components/Details.js
+++ src/components/common_components/Details.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 import "../../common_styles/commonClasses.css";
 const Details = (props) => {
   return (
-    <div className='reveal'>
+    <div>
+      <div className="heading">Urja Global Limited {props.heading_title}</div>
 
-      <div className='heading'>Urja Global Limited {props.heading_title}</div>
-
-      <div className='sub-heading'>Urja Global Limited is in one of India’s leading Renewable Energy developers.</div>
-      <p className='paragraph-css'>Urja Global Limited is in one of India’s leading Renewable Energy developers and operators. Urja is engaged in design, consultancy, integration, supply, installation, commissioning & maintenance of off-grid and grid connected Solar Power Plants. We are approved Channel Partners of Ministry of New and Renewable Energy (MNRE), Govt. of India.</p>
-
-
-
+      <div className="sub-heading">
+        Urja Global Limited is in one of India’s leading Renewable Energy
+        developers.
+      </div>
+      <p className="paragraph-css">
+        Urja Global Limited is in one of India’s leading Renewable Energy
+        developers and operators. Urja is engaged in design, consultancy,
+        integration, supply, installation, commissioning & maintenance of
+        off-grid and grid connected Solar Power Plants. We are approved Channel
+        Partners of Ministry of New and Renewable Energy (MNRE), Govt. of India.
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Details;
