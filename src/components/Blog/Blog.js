c393bfaa14b59654b1ef020ab80249ca727e5d47
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2022-03-27 15:45:52 +0530

    styling-correction

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,11 +8,11 @@ const Blog = () => {
     return (
         <>
             {/* <div className=''><img src={require('../../assets/blog-hero.jpg')} alt='' style={{ width: '100%',height:'2rem' }} /> </div> */}
-            <div className='common-heading hero-image' style={{ fontWeight: '900', marginTop: '3rem', fontSize: '4rem', color: 'var(--primarygreen)' }}>Blog </div>
+            <div className='common-heading hero-image' style={{ fontWeight: '900', fontSize: '4rem', color: 'var(--primarygreen)' }}>Blog </div>
             <div className='max-width'>
 
 
-                <div className='band'>{
+                <div className='cards-flex'>{
                     data.map((curElem) => {
                         return <BlogCard key={curElem.id} {...curElem} />
                         //here the spread operator is use to pass the the properties of the questions api
