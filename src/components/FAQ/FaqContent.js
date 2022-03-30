044ffadb943a282d4a304d4e558ee2b548d44c28
Prakash Chandra Bhatt <77747508+prakashbhattcse@users.noreply.github.com>
2022-03-30 10:07:43 +0530

    changes

--- src/components/FAQ/FaqContent.js
+++ src/components/FAQ/FaqContent.js
@@ -1,11 +1,13 @@
 import React,{useState}  from 'react'
+import { BiMinus } from "react-icons/bi";
+import { BiPlus } from "react-icons/bi";
 
 const FaqContent = ({ question, answer }) => {
     const [show, setShow] = useState(false);
     return (
         <>
            <div className="main-heading">
-                <p onClick={() => setShow(!show)}> { show? "➖" : "➕"} </p>
+                <p style={{marginBottom:'5px'}} onClick={() => setShow(!show)}> { show? <BiMinus color="white" size="1.5rem"/> : <BiPlus size="1.5rem" color="white"/>}  </p>
                 <h3>{question}</h3>
                    
             </div>
